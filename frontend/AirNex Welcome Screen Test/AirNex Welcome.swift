e8f2235d4d896c7230af8a71bd19d5983583122e
ryhaankhadri@gmail.com <s.r.khadri@se21.qmul.ac.uk>
2024-01-13 16:34:05 +0000

    Inital Commit
    Initial test files and folder structure pushed. Basic Frontend test for the welcome screen implemented with no functionality so far.

---
+++ frontend/AirNex Welcome Screen Test/AirNex Welcome.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Ryhaan Khadri on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
